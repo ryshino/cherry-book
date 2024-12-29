055d9bece8f84976a65a815f09bad8ceaed639ab
ryshino <ranature1153@gmail.com>
2024-12-29 18:38:53 +0900

    繰り返し処理を学ぶ

--- sample.rb
+++ sample.rb
@@ -1,27 +1,63 @@
-fruits = ['apple', 'orange', 'melon']
-p fruits.each
+a = []
+10.upto(14) { |n| a << n }
+p a
 
-dimensions = [
-  [10, 20],
-  [30, 40],
-  [50, 60],
-]
-areas = []
-dimensions.each do |length, width|
-  areas << length * width
+a = []
+14.downto(10) { |n| a << n }
+p a
+
+def factorial(n)
+  n > 0 ? n * factorial(n - 1) : 1
 end
-p areas
 
-(length, width), i = [[10, 20], 0]
-p length
-p width
-p i
+p factorial(5)
 
-p ['1', '20', '300'].map { _1.rjust(3, '0') }
+def factorial(n)
+  ret = 1
+  (1..n).each { |n| ret *= n }
+  ret
+end
+p factorial(5)
+p factorial(0)
 
-sum = 0
-[[1, 2, 3,], [4, 5, 7]].each do |values|
-  values.each do
-    sum += _1
+numbers = [1, 2, 3, 4, 5].shuffle
+numbers.each do |n|
+  puts n
+  break if n == 5
+end
+
+fruits = ['apple', 'melon', 'orange']
+numbers = [1, 2, 3]
+fruits.each do |fruit|
+  numbers.shuffle.each do |n|
+    puts "#{fruit}, #{n}"
+    break if n == 3
   end
-end　
+end
+
+fruits = ['apple', 'melon', 'orange']
+numbers = [1, 2, 3]
+catch :done do
+  fruits.shuffle.each do |fruit|
+    numbers.shuffle.each do |n|
+      puts "#{fruit}, #{n}"
+      if fruit == 'orange' && n == 3
+        throw :done
+      end
+    end
+  end
+end
+
+foods = ['ピーマン', 'トマト', 'セロリ']
+count = 0
+
+foods.each do |food|
+  print "#{food}は好きですか？"
+  answer = ['はい', 'いいえ'].sample
+  answer = 'いいえ'
+  puts answer
+
+  count += 1
+  redo if answer != 'はい' && count < 2
+  count = 0
+end
