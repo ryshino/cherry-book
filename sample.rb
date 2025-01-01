83b7ab425179788d16012e805eb0581433a64721
ryshino <ranature1153@gmail.com>
2025-01-01 17:29:02 +0900

    正規表現の勉強

--- sample.rb
+++ sample.rb
@@ -1,13 +1,9 @@
-h = Hash.new('hello')
-a = h[:foo]
-b = h[:bar]
-
-p h
-
-p %s!ruby is fun!
-p %s(ruby is fun)
-p %i(apple orange melon)
-
-name = 'Alice'
-p %i(hello\ngood-bye #{name.upcase})
-p %I(hello\ngood-bye #{name.upcase})
+text = <<-TEXT
+名前：伊藤淳一
+電話：03(1234)5678
+電話：090-1234-5678
+電話：0795(12)3456
+電話：04992-1-2345
+住所：兵庫県西脇市板波町1-2-3
+TEXT
+p text.scan /\d{2,5}[-(]\d{1,4}[-)]\d{4}/
