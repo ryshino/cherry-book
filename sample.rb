518b30dcf9731962294ddcda5fee2bbd23cb7a72
ryshino <ranature1153@gmail.com>
2025-01-02 18:03:00 +0900

    正規表現での置換について学ぶ

--- sample.rb
+++ sample.rb
@@ -1,10 +1,12 @@
-text = <<-TEXT
-クープバゲットのパンは美味しかった。
-今日はクープ バゲットさんに行きました。
-クープ　バゲットのパンは最高。
-ジャムおじさんのパン、ジャムが入ってた。
-また行きたいです。クープ・バゲット。
-クープ・バケットのパン、売り切れだった（><）
-TEXT
+html = <<-HTML
+<select name="game_console">
+<option value="none"></option>
+<option value="wii_u" selected>Wii U</option>
+<option value="ps4">プレステ4</option>
+<option value="gb">ゲームボーイ</option>
+</select>
+HTML
 
-p text.split(/\n/).grep(/クープ.?バ[ゲケ]ット/)
+replaced = html.gsub(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/, '\1\2')
+
+puts replaced
