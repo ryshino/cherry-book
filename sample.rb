4d8b29d0b2ab98c87966b1d7ab910686de9634aa
ryshino <ranature1153@gmail.com>
2025-01-08 11:17:56 +0900

    キャプチャに名前をつける

--- sample.rb
+++ sample.rb
@@ -1,6 +1,12 @@
-text = <<-TEXT
-John:guitar, George:guitar, Paul:bass, Ringo:drum
-Freddie:vocal, Brian:guitar, John:bass, Roger:drum
-TEXT
-p text.scan(/\w+(?=:bass)/)
-# => ["Paul", "John"]
+text = '私の誕生日は1997年7月17日です。'
+m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
+p m[:year]
+p m[:month]
+p m[:day]
+# 文字列もOK
+p m['year']
+
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+  puts text
+  puts "#{year}/#{month}/#{day}"
+end
