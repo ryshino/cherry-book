311015d6b1da1a81a13f3ad88ae098b5e830c38c
ryshino <ranature1153@gmail.com>
2025-01-12 16:08:16 +0900

    xオプションを試してみる

--- sample.rb
+++ sample.rb
@@ -1,4 +1,6 @@
-text = '誕生日は1997年7月17日です'
-text.gsub(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/) do
-  "#{$~[:year]}-#{$~[:month]}-#{$~[:day]}"
-end
+regexp = /
+  \d{3}
+  \ 
+  \d{4}
+/x
+'123 4576' =~ regexp
