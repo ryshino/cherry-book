3b28ada88959b5bee1b4b421696184cb41069c37
ryshino <ranature1153@gmail.com>
2025-01-13 11:27:04 +0900

    Userクラスを作成してみる

--- sample.rb
+++ sample.rb
@@ -1,6 +1,21 @@
-regexp = /
-  \d{3}
-  \ 
-  \d{4}
-/x
-'123 4576' =~ regexp
+class User
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+
+  def self.create_users(names)
+    names.map do |name|
+      User.new(name)
+    end
+  end
+
+  def hello
+    "Hello, I am #{@name}"
+  end
+end
+names = ['Alice', 'Bob', 'Carol']
+users = User::create_users(names)
+users.each do |user|
+  puts user.hello
+end
