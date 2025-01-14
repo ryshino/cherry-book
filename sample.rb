355a1e8a79f679eed1d5d7f4dcd94c77ed0ea08a
ryshino <ranature1153@gmail.com>
2025-01-14 10:59:36 +0900

    クラスの継承について学ぶ

--- sample.rb
+++ sample.rb
@@ -1,21 +1,33 @@
-class User
-  attr_accessor :name
-  def initialize(name)
+class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
     @name = name
+    @price = price
   end
 
-  def self.create_users(names)
-    names.map do |name|
-      User.new(name)
-    end
+  def to_s
+    "name: #{name}, price: #{price}"
+  end
+  
+end
+
+class DVD < Product
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    super(name, price)
+    @running_time = running_time
   end
 
-  def hello
-    "Hello, I am #{@name}"
+  def to_s
+    "#{super}, running_time: #{running_time}"
   end
+  
 end
-names = ['Alice', 'Bob', 'Carol']
-users = User::create_users(names)
-users.each do |user|
-  puts user.hello
-end
+
+dvd = DVD.new('A great movie', 1000, 120)
+p dvd.name
+p dvd.price
+p dvd.running_time
+p dvd.to_s
