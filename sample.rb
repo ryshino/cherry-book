94ed8e203daa394330c223b01e3c11a446af0cca
ryshino <ranature1153@gmail.com>
2025-01-15 13:24:44 +0900

    メソッドのオーバーライドについて学ぶ

--- sample.rb
+++ sample.rb
@@ -1,33 +1,28 @@
-class Product
-  attr_reader :name, :price
+class CountryCode
+  attr_reader :code
 
-  def initialize(name, price)
-    @name = name
-    @price = price
+  def initialize(code)
+    @code = code
   end
 
-  def to_s
-    "name: #{name}, price: #{price}"
+  def eql?(other)
+    puts "eql?メソッドが呼ばれました"
+    other.instance_of?(CountryCode) && code.eql?(other.code)
   end
-  
+
+  def hash
+    # ここのcodeは@codeを返すattr_readerで定義されたメソッド
+    # つまりレシーバ（self）のインスタンス変数@codeを参照している
+    puts "hashメソッドが呼ばれました"
+    puts "レシーバのオブジェクトID: #{self.object_id}"
+    puts "codeのオブジェクトID: #{code.object_id}"
+    puts "codeのhash値: #{code.hash}"
+    code.hash
+  end
 end
 
-class DVD < Product
-  attr_reader :running_time
+japan = CountryCode.new('JP')
+currencies = { japan => 'yen' }
 
-  def initialize(name, price, running_time)
-    super(name, price)
-    @running_time = running_time
-  end
-
-  def to_s
-    "#{super}, running_time: #{running_time}"
-  end
-  
-end
-
-dvd = DVD.new('A great movie', 1000, 120)
-p dvd.name
-p dvd.price
-p dvd.running_time
-p dvd.to_s
+hoge = CountryCode.new('JP')
+p currencies[hoge]
