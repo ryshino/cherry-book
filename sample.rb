bb5e5e15a46c82e774413f13a0ab71cbbe76dded
ryshino <ranature1153@gmail.com>
2025-01-20 09:14:35 +0900

    例外を捕捉する

--- sample.rb
+++ sample.rb
@@ -1,20 +1,9 @@
-# このクラスは外部ライブラリに定義されてる想定
-class Product
-  def name
-    'A great film'
-  end
+begin
+  1 /0
+rescue => e
+  puts "エラークラス: #{e.class}"
+  puts "エラーメッセージ: #{e.message}"
+  puts "バックトレース -----"
+  puts e
+  puts "-----"
 end
-
-module NameDecorator
-  def name
-    "<<#{super}>>"
-  end
-end
-
-class Product
-  prepend NameDecorator
-end
-
-product = Product.new
-p product.name
-p Product.ancestors
