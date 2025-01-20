aa6ccfb2251022b4a948c81d17bad0eeb53944a3
ryshino <ranature1153@gmail.com>
2025-01-20 11:54:24 +0900

    Procオブジェクトを普通の引数として渡す

--- sample.rb
+++ sample.rb
@@ -1,24 +1,11 @@
-def greet_ja(&block)
-  texts = ['おはよう', 'こんにちは', 'こんばんは']
-  # ブロックを別のメソッドに渡す
-  greet_common(texts, &block)
+def greet(proc_1, proc_2, proc_3)
+  puts proc_1.call('おはよう')
+  puts proc_2.call('こんにちは')
+  puts proc_3.call('こんばんは')
 end
 
-def greet_en(&block)
-  texts = ['good morning', 'hello', 'good evening']
-  # ブロックを別のメソッドに渡す
-  greet_common(texts, &block)
-end
+shuffle_proc = Proc.new { |text| text.chars.shuffle.join }
+repeat_proc = Proc.new { |text| text * 2 }
+question_proc = Proc.new { |text| "#{text}?" }
 
-def greet_common(texts, &block)
-  puts texts[0]
-  puts block.call(texts[1])
-  puts texts[2]
-end
-
-greet_ja do |text|
-  text * 2
-end
-greet_en do |text|
-  text.upcase
-end
+greet(shuffle_proc, repeat_proc, question_proc)
