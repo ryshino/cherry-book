118ea1ac84d1210dd1ceae04c4061fcf67ff3685
ryshino <ranature1153@gmail.com>
2025-01-21 15:51:48 +0900

    findパターンについて学ぶ

--- sample.rb
+++ sample.rb
@@ -1,16 +1,19 @@
-cars = [
-  {name: 'The Beatle', engine: '105ps'},
-  {name: 'Prius', engine: '98ps', motor: '72ps'},
-  {name: 'Tesla', motor: '306ps'}
+case ["a", 1, "b", "c", 2]
+in [*head, String => s1, String => s2, *tail]
+  puts "最初の文字列: #{s1}"  # => "b"
+  puts "次の文字列: #{s2}"    # => "c"
+  puts "head: #{head}"        # => ["a", 1]
+  puts "tail: #{tail}"        # => [2]
+end
+
+data = [
+  {type: "user", name: "Alice"},
+  {type: "admin", name: "Bob"},
+  {type: "user", name: "Charlie"}
 ]
 
-cars.each do |car|
-  case car
-  in {name:, engine:, motor:}
-    puts "Hybrid: #{name} / engine: #{engine} / motor: #{motor}"
-  in {name:, engine:}
-    puts "Gasoline: #{name} / engine: #{engine}"
-  in {name:, motor:}
-    puts "EV: #{name} / motor: #{motor}"
-  end
+# adminを探す
+case data
+in [*, {type: "admin", name: name}, *]
+  puts "管理者: #{name}"  # => "管理者: Bob"
 end
