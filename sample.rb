79762c7cc8e009e2acbc9860d7b9880955c484ac
ryshino <ranature1153@gmail.com>
2025-01-21 11:43:29 +0900

    case文でprocを扱う

--- sample.rb
+++ sample.rb
@@ -1,8 +1,17 @@
-add_proc = Proc.new { |a, b| a.to_i + b.to_i }
-add_proc.call(10)
-add_proc.call(10, 20, 30)
+def judge(age)
+  adult = Proc.new { |n| n > 20 }
+  child = Proc.new { |n| n < 20 }
 
-add_lambda = -> (a, b) { a.to_i + b.to_i }
-# ラムダは個数について厳密なので、引数が2個ちょうどでなければエラーになる
-add_lambda.call(10)
-add_lambda.call(10, 20, 30)
+  case age
+  when adult
+    '大人です'
+  when child
+    '子どもです'
+  else
+    'はたちです'
+  end
+end
+
+puts judge(25)
+puts judge(18)
+puts judge(20)
