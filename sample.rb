b6d12ac6ee4decc23d947fdab8f0d551be62949b
ryshino <ranature1153@gmail.com>
2025-01-22 10:09:45 +0900

    ガード式を書いてみる

--- sample.rb
+++ sample.rb
@@ -1,19 +1,7 @@
-case ["a", 1, "b", "c", 2]
-in [*head, String => s1, String => s2, *tail]
-  puts "最初の文字列: #{s1}"  # => "b"
-  puts "次の文字列: #{s2}"    # => "c"
-  puts "head: #{head}"        # => ["a", 1]
-  puts "tail: #{tail}"        # => [2]
+hoge = case [1, 2, 3, 2, 1]
+in [*, n , 2, *] if n == 3
+  "matched: #{n}"
+else
+  "not matched"
 end
-
-data = [
-  {type: "user", name: "Alice"},
-  {type: "admin", name: "Bob"},
-  {type: "user", name: "Charlie"}
-]
-
-# adminを探す
-case data
-in [*, {type: "admin", name: name}, *]
-  puts "管理者: #{name}"  # => "管理者: Bob"
-end
+p hoge
